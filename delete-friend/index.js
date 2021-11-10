ddefd10c4a0194216396c59c83577523c45ea540
George Mead <gmeadiv@gmail.com>
2021-11-10 13:21:42 -0800

    adds delete fx that finds by id

---
+++ delete-friend/index.js
@@ -0,0 +1,49 @@
+const dynamoose = require('dynamoose');
+
+
+exports.handler = async (event) => {
+
+  const friendSchema = new dynamoose.Schema({
+    'id': Number,
+    'name': String,
+    'role': String
+  });
+
+  const friendsTable = dynamoose.model('friends', friendSchema)
+
+  let data = null;
+  let status = 500;
+  let id = parseInt(event.pathParameters.id);
+
+  console.log(id, '<-- ID --<<');
+
+  let foundFriend = null;
+
+  try {
+    data = await friendsTable.scan().exec();
+
+    data.map(friend => {
+
+      if (friend.id === id) {
+
+        foundFriend = friend;
+
+      }
+
+    })
+
+    status = 200;
+  } catch (error) {
+    console.log(error, '<-- error --<<');
+
+    data = new Error(error);
+    status = 400;
+  }
+
+  const response = {
+      statusCode: status,
+      body: JSON.stringify(foundFriend),
+  };
+
+  return response;
+};
